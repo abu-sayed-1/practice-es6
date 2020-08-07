10034ebfd46ee9aaf80241b5dd5f10ba119e2d99
as ahmed <rakibasrr13@gmail.com>
2020-08-07 14:14:19 +0300

    null-vs-undefined

---
+++ undefinedVsNull.js
@@ -0,0 +1,19 @@
+let num ;
+console.log(num);
+
+function add(num1,num2){
+    console.log(num1+num2);
+    return
+}
+ const result = add(20,3);
+ console.log(result);
+// ------------------
+const prs = {name:"rakib",phone:103948209,id:232}
+console.log(prs.schollName);
+
+// --------------------------
+let fun = undefined;
+console.log(fun);
+
+let ages = [23,22,21,43]
+console.log(ages[44]);
